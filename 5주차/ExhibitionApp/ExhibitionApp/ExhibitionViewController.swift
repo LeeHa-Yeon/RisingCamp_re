d8b71a91a5578796b66d11b08231d85f9571bf8e
LeeHa-Yeon <gkdus1613@naver.com>
2021-11-25 18:35:18 +0900

    [POD] SnapKit,Then pod install

---
+++ 5주차/ExhibitionApp/ExhibitionApp/ExhibitionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExhibitionViewController.swift
+//  ExhibitionApp
+//
+//  Created by 이하연 on 2021/11/25.
+//
+
+import UIKit
+
+class ExhibitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+    }
+}
