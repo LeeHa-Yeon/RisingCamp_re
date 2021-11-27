25f7dbe9aa79dc80fe81aced327a4b3ba12b62e6
LeeHa-Yeon <gkdus1613@naver.com>
2021-11-27 23:52:15 +0900

    [ADD-6주차] 6주차 프로젝트 초기설정

---
+++ 6주차/ThreadGame/ThreadGame/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreadGame
+//
+//  Created by 이하연 on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
