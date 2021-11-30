fe1ad9bdf5546a67eb6fc4236d77298119f2b2ab
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-01 06:42:55 +0900

    [FEAT] 시작 화면 구현

--- 6주차/ThreadGame/ThreadGame/Applications/SceneDelegate.swift
+++ 6주차/ThreadGame/ThreadGame/Applications/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = .systemBackground
         
-        window?.rootViewController = GameViewController()
+        window?.rootViewController = StartViewController()
         window?.makeKeyAndVisible()
     }
     
