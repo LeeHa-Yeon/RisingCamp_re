130747aaaf582974b3eb8864c7ed7467826f1626
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-05 23:31:51 +0900

    [ADD 7주차] 7주차 프로젝트 설정

--- 6주차/ThreadGame/ThreadGame/Resource/Constatns/Constatns.swift
+++ 6주차/ThreadGame/ThreadGame/Resource/Constatns/Constatns.swift
@@ -6,10 +6,14 @@
 //
 
 struct Constatns {
+    // 게임에 관련된 부분
     static let GAME_TIME:Int = 60
     static let PART_TIME:Int = 10
     
+    // UserDefaults
     static let USER_NICNAME:String = "userNicName"
     static let RANK_INFO: String = "rankInfo"
+    
+    // 게임창에서 dismiss할 때 닉네임 알림창이 안나타나기 위한 구분
     static var isStart: Bool = false
 }
---
+++ 6주차/ThreadGame/ThreadGame/Sources/Scenes/Setting/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  ThreadGame
+//
+//  Created by 이하연 on 2021/12/05.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
