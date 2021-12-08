e241357f43ffaaee03ffc3fcda8e832d5467635d
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-08 18:34:17 +0900

    [FEAT] 설정 화면 배경음악 추가 및 볼륨 조절

--- 6주차/ThreadGame/ThreadGame/Sources/Scenes/Setting/SettingViewController.swift
+++ 6주차/ThreadGame/ThreadGame/Sources/Scenes/Setting/SettingViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import AVFoundation
 
 class SettingViewController: UIViewController {
+    
+    // AVAudioPlayer 인스턴스 참조체 저장
+    var BGM: AVAudioPlayer?
     
     //MARK: - Components
     private lazy var confirmBtn = UIButton().then {
@@ -17,11 +23,21 @@ class SettingViewController: UIViewController {
         $0.setTitleColor(.white, for: .normal)
         $0.addTarget(self, action: #selector(confirmBtnTapped), for: .touchUpInside)
     }
-
+    
+    private lazy var volumeControl = UISlider(frame: CGRect(x:0, y:0, width:200, height:30)).then {
+        $0.minimumValue = 0
+        $0.maximumValue = BGM?.volume ?? 1
+        $0.value = BGM?.volume ?? 1
+        $0.minimumTrackTintColor = UIColor.lightGray
+        $0.maximumTrackTintColor = UIColor.black
+        $0.addTarget(self, action: #selector(volumeUpDown), for: .valueChanged)
+    }
+    
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        playAudio()
     }
     
     override func viewDidLayoutSubviews() {
@@ -34,7 +50,14 @@ class SettingViewController: UIViewController {
     func setUI(){
         view.layer.cornerRadius = 20
         view.backgroundColor = .gray
+        view.addSubview(volumeControl)
         view.addSubview(confirmBtn)
+        
+        volumeControl.snp.makeConstraints{
+            $0.centerX.equalToSuperview()
+            $0.trailing.leading.equalToSuperview().inset(40)
+            $0.bottom.equalTo(confirmBtn.snp.top).inset(-30)
+        }
         
         confirmBtn.snp.makeConstraints{
             $0.centerX.equalToSuperview()
@@ -43,11 +66,31 @@ class SettingViewController: UIViewController {
         }
     }
     
+    func playAudio() {
+        let url = Bundle.main.url(forResource: "배경음악", withExtension: "mp3")
+        if let url = url{
+            do {
+                BGM = try AVAudioPlayer(contentsOf: url)
+                guard let sound = BGM else { return }
+                sound.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        } else {
+            print("file not found")
+        }
+    }
     
     //MARK: - objc Functions
     
     @objc func confirmBtnTapped(){
         self.dismiss(animated: true, completion: nil)
     }
-
+    
+    @objc func volumeUpDown(){
+        if BGM != nil {
+            BGM?.volume = volumeControl.value
+        }
+    }
+    
 }
