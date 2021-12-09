ebde9610c5567078ea1efea1e32624ef71f9282b
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-10 03:45:20 +0900

    [POD] lottie pod install

---
+++ 6주차/ThreadGame/ThreadGame/Sources/Scenes/Loding/LodingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LodingViewController.swift
+//  ThreadGame
+//
+//  Created by 이하연 on 2021/12/10.
+//
+
+import UIKit
+
+class LodingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
