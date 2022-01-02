2048d880dd8f58837e5c7b044bb88c2fe7381154
LeeHa-Yeon <gkdus1613@naver.com>
2022-01-03 08:21:06 +0900

    [FEAT] KakaoLogin 구현

--- 9주차/APITest/APITest/Sources/Controllers/LoginViewController.swift
+++ 9주차/APITest/APITest/Sources/Controllers/LoginViewController.swift
@@ -6,11 +6,39 @@
 //
 
 import UIKit
+import KakaoSDKUser
+
 
 class LoginViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func kakaoLogin(_ sender: Any) {
+        
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    UserDefaults.standard.set("KakaoLogin", forKey: "loginStatus")
+
+                    // 화면 이동
+                    let board = UIStoryboard(name: "MeetPlaceSearch", bundle: nil)
+                    let vc = board.instantiateViewController(withIdentifier: "TabSB")
+                    vc.modalPresentationStyle = .fullScreen
+                    self.present(vc, animated: true, completion: nil)
+                    
+                
+                    print("login With KakaoAccount() success.")
+                    
+                    _ = oauthToken
+                }
+            }
+        }
+    }
+    
     
 }
--- 9주차/APITest/APITest/Sources/Controllers/MyPageViewController.swift
+++ 9주차/APITest/APITest/Sources/Controllers/MyPageViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import KakaoSDKUser
+
 
 class MyPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadUserInfo()
 
+    }
+    
+    func loadUserInfo(){
+        if "KakaoLogin" == UserDefaults.standard.string(forKey: "loginStatus") {
+            print("여기야여기")
+            UserApi.shared.me() {(user, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("me() success.")
+                    print("카카오유저이메일\(user?.kakaoAccount?.email)")
+                    print("카카오유저\(user?.kakaoAccount?.profile)")
+                    //do something
+                    _ = user
+                }
+            }
+        }else {
+            print("저기야저기")
+        }
     }
     
 }
