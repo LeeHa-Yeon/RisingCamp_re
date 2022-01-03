2bc1518b1ae6678407e18de079a7ada749e3f8e8
LeeHa-Yeon <gkdus1613@naver.com>
2022-01-03 14:15:02 +0900

    [FEAT] 마이페이지 구현

--- 9주차/APITest/APITest/Sources/Controllers/LoginViewController.swift
+++ 9주차/APITest/APITest/Sources/Controllers/LoginViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import Lottie
 import KakaoSDKUser
 
 
 class LoginViewController: UIViewController {
     
+    let animationView = AnimationView(name:"global")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animationView.frame = CGRect(x:0, y:0, width:200, height:200)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        view.addSubview(animationView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        //애니메이션 시간 후에 작동하게끔.
+        self.animationView.play()
+        self.animationView.loopMode = .loop
     }
     
     @IBAction func kakaoLogin(_ sender: Any) {
@@ -24,14 +39,14 @@ class LoginViewController: UIViewController {
                 }
                 else {
                     UserDefaults.standard.set("KakaoLogin", forKey: "loginStatus")
-
+                    
                     // 화면 이동
                     let board = UIStoryboard(name: "MeetPlaceSearch", bundle: nil)
                     let vc = board.instantiateViewController(withIdentifier: "TabSB")
                     vc.modalPresentationStyle = .fullScreen
                     self.present(vc, animated: true, completion: nil)
                     
-                
+                    
                     print("login With KakaoAccount() success.")
                     
                     _ = oauthToken
--- 9주차/APITest/APITest/Sources/Controllers/MyPageViewController.swift
+++ 9주차/APITest/APITest/Sources/Controllers/MyPageViewController.swift
@@ -7,16 +7,35 @@
 
 import UIKit
 import KakaoSDKUser
-
+import Kingfisher
 
 class MyPageViewController: UIViewController {
-
+    
+    var profileURL : String = ""
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViews()
         loadUserInfo()
-
+        
     }
     
+    @IBAction func logout(_ sender: Any) {
+        UserApi.shared.logout {(error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                let board = UIStoryboard(name: "Login", bundle: nil)
+                let vc = board.instantiateViewController(withIdentifier: "LoginSB")
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true, completion: nil)
+                print("logout() success.")
+            }
+        }
+    }
     func loadUserInfo(){
         if "KakaoLogin" == UserDefaults.standard.string(forKey: "loginStatus") {
             print("여기야여기")
@@ -26,8 +45,13 @@ class MyPageViewController: UIViewController {
                 }
                 else {
                     print("me() success.")
-                    print("카카오유저이메일\(user?.kakaoAccount?.email)")
-                    print("카카오유저\(user?.kakaoAccount?.profile)")
+                    self.emailLabel.text = "\((user?.kakaoAccount?.email)!)"
+                    self.nicknameLabel.text = "\((user?.kakaoAccount?.profile?.nickname)!)"
+                    self.profileURL = "\((user?.kakaoAccount?.profile?.profileImageUrl)!)"
+                    DispatchQueue.main.async {
+                        let url = URL(string: (self.profileURL))
+                        self.profileImg.kf.setImage(with: url)
+                    }
                     //do something
                     _ = user
                 }
@@ -35,6 +59,14 @@ class MyPageViewController: UIViewController {
         }else {
             print("저기야저기")
         }
+        
     }
+    
+    func initViews(){
+        profileImg.layer.cornerRadius = profileImg.frame.size.height/3
+        profileImg.layer.masksToBounds = true
+    }
+    
+    
     
 }
